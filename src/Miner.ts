1e65c0bf0d3dc1ce759abcfbd76720b2a9dc91a1
partharora1610 <partharora16102001@gmail.com>
2024-04-16 23:00:22 +0530

    reversed wtxid

--- src/Miner.ts
+++ src/Miner.ts
@@ -30,8 +30,10 @@ export class Miner {
       weights
     );
 
-    const wTxid = res.map((tx) => tx.getWTxID()); // witness root hash
+    const wTxid = res.map((tx) => tx.getWTxID().reverse().toString("hex")); // witness root hash
     const witnessCommitment = calculateWitnessCommitment(wTxid);
+
+    console.log("witnessCommitment", witnessCommitment);
 
     const coinbaseTx = Tx.createCoinbaseTransaction(witnessCommitment);
     // console.log(coinbaseTx);
